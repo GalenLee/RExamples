3a3488be7df61fe53bded78b70c9044debcff298
GalenLee <glwadzinski@gmail.com>
2016-09-04 18:53:38 -0500

    Create

---
+++ Work.R
@@ -0,0 +1,18 @@
+# Working script
+# Just playing around with commands and packages
+
+install.packages("combinat")
+library()
+library(combinat)
+help(package="combinat")
+?combn
+search()
+utils::combn(1:5,2)
+combinat::combn(1:5,2)
+permn(1:5)
+detach("package:combinat")
+
+install.packages("gtools")
+help(package="gtools")
+library(gtools)
+permutations(5,5)
